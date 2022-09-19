4422a8645dda8cd025c0003bae9881d9631ead27
coltonnakonechny <coltonnakonechny@outlook.com>
2022-09-18 21:14:33 -0600

    my first commit

---
+++ app.js
@@ -0,0 +1,87 @@
+//Par One//
+
+console.log("Hello World");
+
+//Par Two//
+
+let MyName = "Colton";
+let MyAge = 31;
+let MyFavouriteColor = "Black";
+const MyDOB =("October/24th/1990");
+
+//Par Three//
+
+console.log( MyName + " is " + MyAge + "Years Old," + "I'm born on" + MyDOB + " .And my favourite color is" + MyFavouriteColor);
+
+//Par Four//
+
+let MyFriendName = "JamesBond";
+let MyFriendAge = 26;
+let MyFriendFavColor = "Blue";
+
+const MyFriendDOB = "Febuary/8th/1995";
+
+If (MyFriendAge > MyAge)
+{ 
+    console.log(MyFriendName + "is younger than  + MyName);
+}
+
+MyAge (-)=6;
+console.log( MyName + " is " - MyAge + "Years old," + "I'm born on " + DOB + " .And my favourite color is" + MyFavColor);
+
+let sum (MyAge-6) + MyFriendAge;
+console.log(" Sum of " + MyName+ " and " + MyFriendName+" age is - sum);
+
+//Par Five// {Arthimetic Operations - Data Types}
+
+{Addition Operation} (+)
+
+/////////////////////////////////////////////////////
+let sum = 30 + 50;
+
+console.log(sum); // 80
+      :1 80
+/////////////////////////////////////////////////////
+let netPrice = 49.99,
+shippingFee   =3.99
+  
+  let netPrice = 49.99,
+      shippingFee =3.99;
+  
+  let grossPrice = netPrice + shippingFee;
+  
+  console.log(grossPrice);
+  Output:1 53.980000000000004
+  //////////////////////////////////////////////////
+  let x = '60',
+      y = '40';
+  let result = x + y;
+
+  output=6040
+
+  let result = 60 + '40';
+
+  console.log(result);
+
+  Output:6040
+  //////////////////////////////////////////////////
+
+{Multiplaction Operation (x-*)}
+
+////////////////////////////////////////////////////
+
+console.log(result);
+Output:1 6040
+
+let result = 777 * 306;
+
+console.log(result);
+Output:1 237762
+
+let result = '777' * 306;
+
+console.log(result);
+Output:1 237762
+
+////////////////////////////////////////////////////
+
